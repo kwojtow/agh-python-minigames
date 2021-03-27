b5827b7b5229503f0488e827922d47970af32b91
Krzysztof Wójtowicz <k.wojtowicz.03@gmail.com>
2021-03-27 22:20:26 +0100

    Branches merged manualy

--- networking.py
+++ networking.py
@@ -22,13 +22,13 @@ class Network:
             raise
 
     def current_minigame(self):
-        return self.send("gameinfo")[0]
+        return self.send_recv("gameinfo")[0]
 
     def score(self):
-        return self.send("gameinfo")[1:]
+        return self.send_recv("gameinfo")[1:]
 
     def get_data(self):
-        return self.send("get")
+        return self.send_recv("get")
 
     def game_won_by(self,player_nmbr):
         if player_nmbr==0:
@@ -36,10 +36,15 @@ class Network:
         elif player_nmbr==1:
             self.send("p1w")
 
-    def send(self, data):
+    def send_recv(self, data):
         try:
             self.client.send(pickle.dumps(data))
             return pickle.loads(self.client.recv(2048*3))
         except socket.error as e:
             print(e)
 
+    def send(self, data):
+        try:
+            self.client.send(pickle.dumps(data))
+        except socket.error as e:
+            print(e)
